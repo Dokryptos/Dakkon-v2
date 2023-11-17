8f3878814851100264eae3d6aa878a7fed1ae4ec
Dok <pierredok@outlook.fr>
2023-11-17 17:22:59 +0100

    working with gsap for all the animation

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,17 +1,34 @@
-import video3D from '../Assets/travel_1.mp4'
-import  "./VideoPlayer.css";
+import {useRef, useEffect} from 'react';
+import 3D_Video from './Assets/travel_1.mp4';
+import { gsap } from 'gsap/gsap-core';
+import { ScrollTrigger } from 'gsap/all';
+
+import './VideoPlayer.css';
 
 const VideoPlayer = () => {
 
+    const videoRef = useRef(null);
+    gsap.registerPlugin(ScrollTrigger);
 
+    useEffect(() => {
+        const video = videoRef.current;
 
-    return (
-        <div >
-            <video className="video"  controls>
-                <source src={video3D} type="video/mp4" />
-            </video>
-        </div>
-    );
+        let tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'video',
+                start: "top top",
+                end: 'bottom+=200% bottom',
+                scrub: 2,
+                markers: true
+            }
+        })
+    })
+
+    return(
+        <>
+
+        </>
+    )
 };
 
 export default VideoPlayer
